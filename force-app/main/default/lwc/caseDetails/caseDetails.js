3f4390e8ae1318567320052e675c4de40ce94535
Fahad javed <javedfahad040@gmail.com>
2024-09-03 18:36:02 +0530

    done

--- force-app/main/default/lwc/caseDetails/caseDetails.js
+++ force-app/main/default/lwc/caseDetails/caseDetails.js
@@ -1,25 +1,3 @@
-// import { LightningElement, api, wire } from 'lwc';
-// import getCaseDetails from '@salesforce/apex/CaseDetailsApexController.getCaseDetails';
-
-// export default class CaseDetails extends LightningElement {
-//     @api caseId;
-//     caseDetails;
-//     error;
-
-//     @wire(getCaseDetails, { caseId: '$caseId' })
-//     wiredCase({ error, data }) {
-//         if (data) {
-//             this.caseDetails = data;
-//             this.error = undefined;
-//         } else if (error) {
-//             this.caseDetails = undefined;
-//             this.error = 'Error fetching case details: ' + error.body.message;
-//         }
-//     }
-// }
-
-
-
 import { LightningElement, api, wire } from 'lwc';
 import getCaseDetails from '@salesforce/apex/CaseDetailsApexController.getCaseDetails';
 
@@ -37,5 +15,9 @@ export default class CaseDetails extends LightningElement {
             this.caseDetails = undefined;
             this.error = 'Error fetching case details: ' + error.body.message;
         }
+    }
+
+    get caseUrl() {
+        return `/lightning/r/Case/${this.caseDetails.Id}/view`;
     }
 }
